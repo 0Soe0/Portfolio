dcd37a3564f72969bebee871625613e3af69dd17
Soe <so-phi-e@hotmail.com>
2026-07-07 22:40:09 -0300

    added two buttons to download the academic transcripts for both the web dev diploma and the career as a whole in the Certificates section and updated the paragraph to match said feature

--- src/components/CertificatesSection.tsx
+++ src/components/CertificatesSection.tsx
@@ -1,4 +1,5 @@
 import { ExternalLink } from "lucide-react";
+import { Button } from "./Button";
 
 type Certificate = {
   name: string;
@@ -73,12 +74,30 @@ const CertificateCard = ({ certificate }: { certificate: Certificate }) => (
             My <span className="text-primary">Certificates</span>
         </h2>
         <p className="text-center text-white dark:text-muted-foreground mb-12 max-w-2xl mx-auto">
-        Here are some of the certificates I've earned through online courses I have taken and seminars I have attended.
-      </p>
+        Here are some of the certificates I've earned through online courses I have taken and seminars I have attended. You can also find the academic transcript for my Web Developer diploma and the academic transcript for the career I'm studying as a whole down below to download.
+        </p>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {certificates.map((certificate) => (
             <CertificateCard key={certificate.name} certificate={certificate} />
           ))}
+        </div>
+        <div className="flex flex-col sm:flex-row pt-12 gap-8 justify-center items-center">
+          <Button
+            className="w-fit flex items-center gap-2"
+            href="/WebDevAcademicTranscript.pdf"
+            download
+          >
+            Web dev academic transcript
+          </Button>
+
+          <Button
+          className="w-fit flex items-center gap-2"
+          hollow
+          href="/CareerAcademicTranscript.pdf"
+          download
+          >
+            Career academic transcript
+          </Button>
         </div>
       </div>
       </section>
